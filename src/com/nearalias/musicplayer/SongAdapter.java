b3b1fa2dc148e130812d9d24fe3437b69bea8118
Near Alias <nearalias@gmail.com>
2013-07-01 12:43:36 -0400

    adding song model and adapter for it

---
+++ src/com/nearalias/musicplayer/SongAdapter.java
@@ -0,0 +1,15 @@
+package com.nearalias.musicplayer;
+
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
+import com.nearalias.musicplayer.models.Song;
+
+public class SongAdapter extends ArrayAdapter<Song> {
+
+	public SongAdapter(Context context, int textViewResourceId) {
+		super(context, textViewResourceId);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/com/nearalias/musicplayer/models/Song.java
@@ -0,0 +1,112 @@
+package com.nearalias.musicplayer.models;
+
+public class Song {
+
+	private String name, artist, album, genre, lyrics;
+	
+	public Song() {
+	}
+	
+	public Song(String name, String artist, String album, String genre, String lyrics) {
+		this.name = name;
+		this.artist = artist;
+		this.album = album;
+		this.genre = genre;
+		this.lyrics = lyrics;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getArtist() {
+		return artist;
+	}
+
+	public void setArtist(String artist) {
+		this.artist = artist;
+	}
+
+	public String getAlbum() {
+		return album;
+	}
+
+	public void setAlbum(String album) {
+		this.album = album;
+	}
+
+	public String getGenre() {
+		return genre;
+	}
+
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+
+	public String getLyrics() {
+		return lyrics;
+	}
+
+	public void setLyrics(String lyrics) {
+		this.lyrics = lyrics;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((album == null) ? 0 : album.hashCode());
+		result = prime * result + ((artist == null) ? 0 : artist.hashCode());
+		result = prime * result + ((genre == null) ? 0 : genre.hashCode());
+		result = prime * result + ((lyrics == null) ? 0 : lyrics.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		
+		Song other = (Song) obj;		
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		
+		if (artist == null) {
+			if (other.artist != null)
+				return false;
+		} else if (!artist.equals(other.artist))
+			return false;
+		
+		if (album == null) {
+			if (other.album != null)
+				return false;
+		} else if (!album.equals(other.album))
+			return false;
+		
+		if (genre == null) {
+			if (other.genre != null)
+				return false;
+		} else if (!genre.equals(other.genre))
+			return false;
+		
+		if (lyrics == null) {
+			if (other.lyrics != null)
+				return false;
+		} else if (!lyrics.equals(other.lyrics))
+			return false;
+		
+		return true;
+	}
+}
