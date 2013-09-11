ae242eb025edf5204babcbb532aab73bbf51df30
Near Alias <nearalias@gmail.com>
2013-09-10 23:31:50 -0400

    content provider refactoring

--- src/com/nearalias/musicplayer/providers/MusicPlayerContentProvider.java
+++ src/com/nearalias/musicplayer/providers/MusicPlayerContentProvider.java
@@ -19,27 +19,35 @@ import com.nearalias.musicplayer.database.SongTable;
 public class MusicPlayerContentProvider extends ContentProvider {
 
 	private MusicPlayerDatabaseHelper databaseHelper;
+	private SQLiteDatabase database;
 
 	private static final String AUTHORITY = "com.nearalias.musicplayer.providers";
 
+	// table paths
 	private static final class Paths {
 		private static final String SONG_PATH = "song_uri";
 		private static final String PLAYLIST_PATH = "playlist_uri";
 		private static final String SONG_IN_PLAYLIST_PATH = "song_in_playlist_uri";
 	}
 
+	// content URIs
 	public static final Uri SONG_CONTENT_URI = Uri.parse("content://" + AUTHORITY + "/" + Paths.SONG_PATH);
 	public static final Uri PLAYLIST_CONTENT_URI = Uri.parse("content://" + AUTHORITY + "/" + Paths.PLAYLIST_PATH);
 	public static final Uri SONG_IN_PLAYLIST_CONTENT_URI = Uri.parse("content://" + AUTHORITY + "/" + Paths.SONG_IN_PLAYLIST_PATH);
+
+	// content types
 	public static final String SONG_CONTENT_TYPE = ContentResolver.CURSOR_DIR_BASE_TYPE + "/vnd.nearalias.musicplayer.song";
-	public static final String SONG_CONTENT_ITEM_TYPE = ContentResolver.CURSOR_ITEM_BASE_TYPE + "/vnd.nearalias.musicplayer.song";
 	public static final String PLAYLIST_CONTENT_TYPE = ContentResolver.CURSOR_DIR_BASE_TYPE + "/vnd.nearalias.musicplayer.playlist";
-	public static final String PLAYLIST_CONTENT_ITEM_TYPE = ContentResolver.CURSOR_DIR_BASE_TYPE + "/vnd.nearalias.musicplayer.playlist";
 	public static final String SONG_IN_PLAYLIST_CONTENT_TYPE = ContentResolver.CURSOR_DIR_BASE_TYPE
 			+ "/vnd.nearalias.musicplayer.song_in_playlist";
+
+	// content item types
+	public static final String SONG_CONTENT_ITEM_TYPE = ContentResolver.CURSOR_ITEM_BASE_TYPE + "/vnd.nearalias.musicplayer.song";
+	public static final String PLAYLIST_CONTENT_ITEM_TYPE = ContentResolver.CURSOR_DIR_BASE_TYPE + "/vnd.nearalias.musicplayer.playlist";
 	public static final String SONG_IN_PLAYLIST_CONTENT_ITEM_TYPE = ContentResolver.CURSOR_DIR_BASE_TYPE
 			+ "/vnd.nearalias.musicplayer.song_in_playlist";
 
+	// URI matches
 	private static final class UriMatches {
 		private static final int SONG = 0;
 		private static final int SONG_ID = 1;
@@ -49,6 +57,7 @@ public class MusicPlayerContentProvider extends ContentProvider {
 		private static final int SONG_IN_PLAYLIST_ID = 5;
 	}
 
+	// URI matcher
 	private static final UriMatcher sUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
 	static {
 		sUriMatcher.addURI(AUTHORITY, Paths.SONG_PATH, UriMatches.SONG);
@@ -59,10 +68,20 @@ public class MusicPlayerContentProvider extends ContentProvider {
 		sUriMatcher.addURI(AUTHORITY, Paths.SONG_IN_PLAYLIST_PATH + "/#", UriMatches.SONG_IN_PLAYLIST_ID);
 	}
 
+	private final SQLiteDatabase getDatabase() {
+		if (database == null) {
+			if (databaseHelper == null) {
+				databaseHelper = new MusicPlayerDatabaseHelper(getContext());
+			}
+			database = databaseHelper.getWritableDatabase();
+		}
+		return database;
+	}
+
 	@Override
 	public boolean onCreate() {
 		databaseHelper = new MusicPlayerDatabaseHelper(getContext());
-		return true;
+		return false;
 	}
 
 	@Override
@@ -92,23 +111,22 @@ public class MusicPlayerContentProvider extends ContentProvider {
 	@Override
 	public Uri insert(Uri uri, ContentValues values) {
 		Uri resultUri = null;
-		SQLiteDatabase database = databaseHelper.getWritableDatabase();
 		long id = 0;
 		switch (sUriMatcher.match(uri)) {
 		case UriMatches.SONG:
-			id = database.insert(SongTable.SONG_TABLE_NAME, null, values);
+			id = getDatabase().insert(SongTable.SONG_TABLE_NAME, null, values);
 			if (id > 0) {
 				resultUri = ContentUris.withAppendedId(SONG_CONTENT_URI, id);
 			}
 			break;
 		case UriMatches.PLAYLIST:
-			id = database.insert(PlaylistTable.PLAYLIST_TABLE_NAME, null, values);
+			id = getDatabase().insert(PlaylistTable.PLAYLIST_TABLE_NAME, null, values);
 			if (id > 0) {
 				resultUri = ContentUris.withAppendedId(PLAYLIST_CONTENT_URI, id);
 			}
 			break;
 		case UriMatches.SONG_IN_PLAYLIST:
-			id = database.insert(SongInPlaylistTable.SONG_IN_PLAYLIST_TABLE_NAME, null, values);
+			id = getDatabase().insert(SongInPlaylistTable.SONG_IN_PLAYLIST_TABLE_NAME, null, values);
 			if (id > 0) {
 				resultUri = ContentUris.withAppendedId(SONG_IN_PLAYLIST_CONTENT_URI, id);
 			}
@@ -126,7 +144,6 @@ public class MusicPlayerContentProvider extends ContentProvider {
 	public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
 		// verifyColumnsAreValid(projection);
 
-		SQLiteDatabase database = databaseHelper.getWritableDatabase();
 		SQLiteQueryBuilder queryBuilder = new SQLiteQueryBuilder();
 
 		switch (sUriMatcher.match(uri)) {
@@ -152,7 +169,7 @@ public class MusicPlayerContentProvider extends ContentProvider {
 			throw new IllegalArgumentException("Unknown URI: " + uri);
 		}
 
-		Cursor cursor = queryBuilder.query(database, projection, selection, selectionArgs, null, null, sortOrder);
+		Cursor cursor = queryBuilder.query(getDatabase(), projection, selection, selectionArgs, null, null, sortOrder);
 		cursor.setNotificationUri(getContext().getContentResolver(), uri);
 
 		return cursor;
@@ -160,45 +177,44 @@ public class MusicPlayerContentProvider extends ContentProvider {
 
 	@Override
 	public int delete(Uri uri, String selection, String[] selectionArgs) {
-		SQLiteDatabase database = databaseHelper.getWritableDatabase();
 		int rowsDeleted = 0;
 		String id;
 		switch (sUriMatcher.match(uri)) {
 		case UriMatches.SONG:
-			rowsDeleted = database.delete(SongTable.SONG_TABLE_NAME, selection, selectionArgs);
+			rowsDeleted = getDatabase().delete(SongTable.SONG_TABLE_NAME, selection, selectionArgs);
 			break;
 		case UriMatches.SONG_ID:
 			id = uri.getLastPathSegment();
 			if (TextUtils.isEmpty(selection)) {
-				rowsDeleted = database.delete(SongTable.SONG_TABLE_NAME, SongTable.COLUMN_ID + "=" + id, null);
+				rowsDeleted = getDatabase().delete(SongTable.SONG_TABLE_NAME, SongTable.COLUMN_ID + "=" + id, null);
 			} else {
-				rowsDeleted = database.delete(SongTable.SONG_TABLE_NAME, SongTable.COLUMN_ID + "=" + id + " and " + selection,
+				rowsDeleted = getDatabase().delete(SongTable.SONG_TABLE_NAME, SongTable.COLUMN_ID + "=" + id + " and " + selection,
 						selectionArgs);
 			}
 			break;
 		case UriMatches.PLAYLIST:
-			rowsDeleted = database.delete(PlaylistTable.PLAYLIST_TABLE_NAME, selection, selectionArgs);
+			rowsDeleted = getDatabase().delete(PlaylistTable.PLAYLIST_TABLE_NAME, selection, selectionArgs);
 			break;
 		case UriMatches.PLAYLIST_ID:
 			id = uri.getLastPathSegment();
 			if (TextUtils.isEmpty(selection)) {
-				rowsDeleted = database.delete(PlaylistTable.PLAYLIST_TABLE_NAME, PlaylistTable.COLUMN_ID + "=" + id, null);
+				rowsDeleted = getDatabase().delete(PlaylistTable.PLAYLIST_TABLE_NAME, PlaylistTable.COLUMN_ID + "=" + id, null);
 			} else {
-				rowsDeleted = database.delete(PlaylistTable.PLAYLIST_TABLE_NAME, PlaylistTable.COLUMN_ID + "=" + id + " and " + selection,
-						selectionArgs);
+				rowsDeleted = getDatabase().delete(PlaylistTable.PLAYLIST_TABLE_NAME,
+						PlaylistTable.COLUMN_ID + "=" + id + " and " + selection, selectionArgs);
 			}
 			break;
 		case UriMatches.SONG_IN_PLAYLIST:
-			rowsDeleted = database.delete(SongInPlaylistTable.SONG_IN_PLAYLIST_TABLE_NAME, selection, selectionArgs);
+			rowsDeleted = getDatabase().delete(SongInPlaylistTable.SONG_IN_PLAYLIST_TABLE_NAME, selection, selectionArgs);
 			break;
 		case UriMatches.SONG_IN_PLAYLIST_ID:
 			id = uri.getLastPathSegment();
 			if (TextUtils.isEmpty(selection)) {
-				rowsDeleted = database.delete(SongInPlaylistTable.SONG_IN_PLAYLIST_TABLE_NAME, SongInPlaylistTable.COLUMN_ID + "=" + id,
-						null);
+				rowsDeleted = getDatabase().delete(SongInPlaylistTable.SONG_IN_PLAYLIST_TABLE_NAME,
+						SongInPlaylistTable.COLUMN_ID + "=" + id, null);
 			} else {
-				rowsDeleted = database.delete(SongInPlaylistTable.SONG_IN_PLAYLIST_TABLE_NAME, SongInPlaylistTable.COLUMN_ID + "=" + id
-						+ " and " + selection, selectionArgs);
+				rowsDeleted = getDatabase().delete(SongInPlaylistTable.SONG_IN_PLAYLIST_TABLE_NAME,
+						SongInPlaylistTable.COLUMN_ID + "=" + id + " and " + selection, selectionArgs);
 			}
 			break;
 		default:
@@ -210,45 +226,44 @@ public class MusicPlayerContentProvider extends ContentProvider {
 
 	@Override
 	public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
-		SQLiteDatabase database = databaseHelper.getWritableDatabase();
 		int rowsUpdated = 0;
 		String id;
 		switch (sUriMatcher.match(uri)) {
 		case UriMatches.SONG:
-			rowsUpdated = database.update(SongTable.SONG_TABLE_NAME, values, selection, selectionArgs);
+			rowsUpdated = getDatabase().update(SongTable.SONG_TABLE_NAME, values, selection, selectionArgs);
 			break;
 		case UriMatches.SONG_ID:
 			id = uri.getLastPathSegment();
 			if (TextUtils.isEmpty(selection)) {
-				rowsUpdated = database.update(SongTable.SONG_TABLE_NAME, values, SongTable.COLUMN_ID + "=" + id, null);
+				rowsUpdated = getDatabase().update(SongTable.SONG_TABLE_NAME, values, SongTable.COLUMN_ID + "=" + id, null);
 			} else {
-				rowsUpdated = database.update(SongTable.SONG_TABLE_NAME, values, SongTable.COLUMN_ID + "=" + id + " and " + selection,
+				rowsUpdated = getDatabase().update(SongTable.SONG_TABLE_NAME, values, SongTable.COLUMN_ID + "=" + id + " and " + selection,
 						selectionArgs);
 			}
 			break;
 		case UriMatches.PLAYLIST:
-			rowsUpdated = database.update(PlaylistTable.PLAYLIST_TABLE_NAME, values, selection, selectionArgs);
+			rowsUpdated = getDatabase().update(PlaylistTable.PLAYLIST_TABLE_NAME, values, selection, selectionArgs);
 			break;
 		case UriMatches.PLAYLIST_ID:
 			id = uri.getLastPathSegment();
 			if (TextUtils.isEmpty(selection)) {
-				rowsUpdated = database.update(PlaylistTable.PLAYLIST_TABLE_NAME, values, PlaylistTable.COLUMN_ID + "=" + id, null);
+				rowsUpdated = getDatabase().update(PlaylistTable.PLAYLIST_TABLE_NAME, values, PlaylistTable.COLUMN_ID + "=" + id, null);
 			} else {
-				rowsUpdated = database.update(PlaylistTable.PLAYLIST_TABLE_NAME, values, PlaylistTable.COLUMN_ID + "=" + id + " and "
-						+ selection, selectionArgs);
+				rowsUpdated = getDatabase().update(PlaylistTable.PLAYLIST_TABLE_NAME, values,
+						PlaylistTable.COLUMN_ID + "=" + id + " and " + selection, selectionArgs);
 			}
 			break;
 		case UriMatches.SONG_IN_PLAYLIST:
-			rowsUpdated = database.update(SongInPlaylistTable.SONG_IN_PLAYLIST_TABLE_NAME, values, selection, selectionArgs);
+			rowsUpdated = getDatabase().update(SongInPlaylistTable.SONG_IN_PLAYLIST_TABLE_NAME, values, selection, selectionArgs);
 			break;
 		case UriMatches.SONG_IN_PLAYLIST_ID:
 			id = uri.getLastPathSegment();
 			if (TextUtils.isEmpty(selection)) {
-				rowsUpdated = database.update(SongInPlaylistTable.SONG_IN_PLAYLIST_TABLE_NAME, values, SongInPlaylistTable.COLUMN_ID + "="
-						+ id, null);
+				rowsUpdated = getDatabase().update(SongInPlaylistTable.SONG_IN_PLAYLIST_TABLE_NAME, values,
+						SongInPlaylistTable.COLUMN_ID + "=" + id, null);
 			} else {
-				rowsUpdated = database.update(SongInPlaylistTable.SONG_IN_PLAYLIST_TABLE_NAME, values, SongInPlaylistTable.COLUMN_ID + "="
-						+ id + " and " + selection, selectionArgs);
+				rowsUpdated = getDatabase().update(SongInPlaylistTable.SONG_IN_PLAYLIST_TABLE_NAME, values,
+						SongInPlaylistTable.COLUMN_ID + "=" + id + " and " + selection, selectionArgs);
 			}
 			break;
 		default:
